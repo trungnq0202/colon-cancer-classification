5048b4c902d6d94847c8c49d7bce8cf293a6a198
trungnq0202 <s3742774@rmit.edu.vn>
2021-05-24 00:24:06 +0700

    added VGG16 implementation notebook for cell-type classification

--- base/base_trainer.py
+++
@@ -1,9 +0,0 @@
-class BaseTrain(object):
-    def __init__(self, model, data_train,data_validate,config):
-        self.model = model
-        self.train_data = data_train
-        self.val_data = data_validate
-        self.config = config
-
-    def train(self):
-        raise NotImplementedError
